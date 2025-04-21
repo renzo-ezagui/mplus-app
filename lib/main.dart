63e048dae0a3de9677640f3f56edd47d92db17ea
Renzo <renzo.ezagui@gmail.com>
2025-04-21 16:43:13 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'screens/home_screen.dart';
 import 'screens/practice_screen.dart';
 import 'l10n/app_localizations.dart';
 
@@ -13,19 +12,16 @@ class MPlusApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        fontFamily: 'SansSerif',
-      ),
-      localizationsDelegates: [
+      theme: ThemeData(primarySwatch: Colors.teal),
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('es', ''),
+      ],
+      localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('en', ''),
-        Locale('es', ''),
       ],
       home: PracticeScreen(),
     );
